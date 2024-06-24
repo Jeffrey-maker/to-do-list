f4be6356e408125805dc0319d2cf7cb8db10efc9
Jeffrey-maker <113903389+Jeffrey-maker@users.noreply.github.com>
2024-06-24 17:15:15 -0400

    update frontend localhost

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask_cors import CORS
 from app import create_app
 
 app = create_app()
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://3.133.114.56/:5173"}})
 
 if __name__ == "__main__":
     app.run(debug=True)
